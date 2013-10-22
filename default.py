7e8049e276c8fd6d8b905133e7b30af86b54e2bd
robwebset@gmail.com <robwebset@users.noreply.github.com>
2013-10-22 06:03:50 +0000

    Add ability to hide the TvTunes button

--- default.py
+++ default.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # *  This Program is free software; you can redistribute it and/or modify
 # *  it under the terms of the GNU General Public License as published by
 # *  the Free Software Foundation; either version 2, or (at your option)
@@ -17,6 +18,7 @@ import sys
 import os
 import re
 import random
+import sqlite3
 #Modules XBMC
 import xbmcplugin
 import xbmc
@@ -40,9 +42,6 @@ def log(txt):
             message = u'%s: %s' % (__addonid__, txt)
             xbmc.log(msg=message.encode("utf-8"), level=xbmc.LOGDEBUG)
 
-path = __addon__.getAddonInfo('path').decode("utf-8")
-log("Path: " + path)
-
 ##############################
 # Stores Various Settings
 ##############################
@@ -57,6 +56,8 @@ class Settings():
 
     @staticmethod
     def getExtrasFileTag():
+        if  __addon__.getSetting( "enableFileTag" ) != True:
+            return ""
         return __addon__.getSetting( "extrasFileTag" )
 
     @staticmethod
@@ -75,26 +76,32 @@ class Settings():
     def isMenuReturnInformation():
         return __addon__.getSetting( "extrasReturn" ) == "Information"
 
+    @staticmethod
+    def getAddonVersion():
+        return __addon__.getAddonInfo('version')
 
 ########################################################
 # Class to store all the details for a given extras file
 ########################################################
 class ExtrasItem():
-    def __init__( self, filename ):
+    def __init__( self, directory, filename, isFileMatchExtra=False ):
+        self.directory = directory
         self.filename = filename
+        # Record if the match was by filename rather than in Extras sub-directory
+        self.isFileMatchingExtra = isFileMatchExtra
         # Get the ordering and display details
         (self.orderKey, self.displayName) = self._generateOrderAndDisplay(filename)
 
     # eq and lt defined for sorting order only
     def __eq__(self, other):
         # Check key, display then filename - all need to be the same for equals
-        return ((self.orderKey, self.displayName, self.filename) ==
-                (other.orderKey, other.displayName, other.filename))
+        return ((self.orderKey, self.displayName, self.directory, self.filename, self.isFileMatchingExtra) ==
+                (other.orderKey, other.displayName, other.directory, other.filename, other.isFileMatchingExtra))
 
     def __lt__(self, other):
         # Order in key, display then filename 
-        return ((self.orderKey, self.displayName, self.filename) <
-                (other.orderKey, other.displayName, other.filename))
+        return ((self.orderKey, self.displayName, self.directory, self.filename, self.isFileMatchingExtra) <
+                (other.orderKey, other.displayName, other.directory, other.filename, other.isFileMatchingExtra))
 
     # Returns the name to display to the user for the file
     def getDisplayName(self):
@@ -104,6 +111,14 @@ class ExtrasItem():
     def getFilename(self):
         return self.filename
 
+    def getDirectory(self):
+        return self.directory
+
+    def isFileMatchExtra(self):
+        return self.isFileMatchingExtra
+    
+    def getOrderKey(self):
+        return self.orderKey
 
     def _generateOrderAndDisplay(self, filename):
         # First thing is to trim the display name from the filename
@@ -222,7 +237,7 @@ class VideoExtrasFinder():
     # Gets any extras files that are in the given extras directory
     def getExtrasDirFiles(self, basepath):
         # Add the name of the extras directory to the end of the path
-        extrasDir = os.path.join( basepath, Settings.getExtrasDirName() )
+        extrasDir = os.path.join( basepath, Settings.getExtrasDirName() ).decode("utf-8")
         log( "Checking existence for " + extrasDir )
         extras = []
         # Check if the extras directory exists
@@ -239,8 +254,8 @@ class VideoExtrasFinder():
                 if m:
                     log( "Skiping file: " + filename)
                 else:
-                    extrasFile = os.path.join( extrasDir, filename )
-                    extraItem = ExtrasItem(extrasFile)
+                    extrasFile = os.path.join( extrasDir, filename ).decode("utf-8")
+                    extraItem = ExtrasItem(extrasDir, extrasFile)
                     extras.append(extraItem)
         return extras
 
@@ -249,7 +264,7 @@ class VideoExtrasFinder():
         if xbmcvfs.exists( basepath ):
             dirs, files = xbmcvfs.listdir( basepath )
             for dirname in dirs:
-                dirpath = os.path.join( basepath, dirname )
+                dirpath = os.path.join( basepath, dirname ).decode("utf-8")
                 log( "Nested check in directory: " + dirpath )
                 if( dirname != Settings.getExtrasDirName() ):
                     log( "Check directory: " + dirpath )
@@ -273,8 +288,8 @@ class VideoExtrasFinder():
 
         for aFile in files:
             if extrasTag in aFile:
-                extrasFile = os.path.join( directory, aFile )
-                extraItem = ExtrasItem(extrasFile)
+                extrasFile = os.path.join( directory, aFile ).decode("utf-8")
+                extraItem = ExtrasItem(extrasDir, extrasFile, True)
                 extras.append(extraItem)
         return extras
 
@@ -295,13 +310,16 @@ class VideoExtras():
             self.filename = None
         log( "Root directory: " + self.baseDirectory )
 
-    def run(self):
+    def findExtras(self):
         # Display the busy icon while searching for files
         xbmc.executebuiltin( "ActivateWindow(busydialog)" )
         extrasFinder = VideoExtrasFinder()
         files = extrasFinder.findExtras(self.baseDirectory, self.filename )
         xbmc.executebuiltin( "Dialog.Close(busydialog)" )
-        
+        return files
+
+
+    def run(self, files):
         # All the files have been retrieved, now need to display them
         extrasWindow = VideoExtrasWindow()
         if not files:
@@ -312,9 +330,161 @@ class VideoExtras():
             extrasWindow.showError()
 
 
+#################################
+# Class to handle database access
+#################################
+
+#####
+##### THE FOLLOWING CLASS IS JUST A TEST CLASS AT THE MOMENT
+##### IF IS NOT CALLED FOR THE LIVE SYSTEM
+#####
+
+class ExtrasDB():
+    def __init__( self ):
+        # Start by getting the database location
+        self.configPath = xbmc.translatePath(__addon__.getAddonInfo('profile'))
+        self.databasefile = os.path.join(self.configPath, "extras_database.db").decode("utf-8")
+        log("ExtrasDB: Database file location = " + self.databasefile)
+
+
+    def cleanDatabase(self):
+        # If the database file exists, delete it
+        if xbmcvfs.exists(self.databasefile):
+            xbmcvfs.delete(self.databasefile)
+    
+    def createDatabase(self):
+        # Make sure the database does not already exist
+        if not xbmcvfs.exists(self.databasefile):
+            # Get a connection to the database, this will create the file
+            conn = sqlite3.connect(self.databasefile)
+            c = conn.cursor()
+            
+            # Create the version number table, this is a simple table
+            # that just holds the version details of what created it
+            # It should make upgrade later easier
+            c.execute('''CREATE TABLE version (version text primary key)''')
+            
+            # Insert a row for the version
+            versionNum = Settings.getAddonVersion()
+            log("Version number = " + versionNum)
+            # Run the statement passing in an array with one value
+            c.execute("INSERT INTO version VALUES (?)", (versionNum,))
+
+            # Create a table that will be used to store each extras file
+            # The "id" will be auto-generated as the primary key
+            c.execute('''CREATE TABLE ExtrasFile (id integer primary key, filename text, order_key text, display_name text)''')
+            
+            c.execute('''CREATE TABLE ExtrasDir (id integer primary key, path text, filename text, order_key text, display_name text)''')
+            
+            # Lookup table between the Movie or TV path/filename and the supported extras
+            c.execute('''CREATE TABLE VideoExtrasFileMap (id integer primary key, video_source text, extras_id integer)''')
+
+            # Save (commit) the changes
+            conn.commit()
+
+            # We can also close the connection if we are done with it.
+            # Just be sure any changes have been committed or they will be lost.
+            conn.close()
+        else:
+            # Check if this is an upgrade
+            conn = sqlite3.connect(self.databasefile)
+            c = conn.cursor()
+            c.execute('SELECT * FROM version')
+            log("Current version number in DB is: " + c.fetchone()[0])
+            conn.close()
+
+    def insertExtrasItem(self, extrasItem):
+
+        conn = sqlite3.connect(self.databasefile)
+        c = conn.cursor()
+
+        # TODO: Do a select to see if the extras file already exists in the database
+
+        # Insert one at a time so we can get the ID of each
+        if extrasItem.isFileMatchExtra() :
+            insertData = (extrasItem.getFilename(), extrasItem.getOrderKey(), extrasItem.getDisplayName())
+            c.execute('''INSERT INTO ExtrasFile(filename, order_key, display_name) VALUES (?,?,?)''', insertData)
+        else:
+            insertData = (extrasItem.getDirectory(),extrasItem.getFilename(), extrasItem.getOrderKey(), extrasItem.getDisplayName())
+            c.execute('''INSERT INTO ExtrasDir(path, filename, order_key, display_name) VALUES (?,?,?,?)''', insertData)
+
+        rowId = c.lastrowid
+        conn.commit()
+        conn.close()
+        
+        return rowId
+
+    def insertVideoExtrasFileMap(self, sourceVal, extrasId):
+        insertData = (sourceVal, extrasId)
+        
+        conn = sqlite3.connect(self.databasefile)
+        c = conn.cursor()
+ 
+        c.execute('''INSERT INTO VideoExtrasFileMap(video_source, extras_id) VALUES (?,?)''', insertData)
+        rowId = c.lastrowid
+        conn.commit()
+        conn.close()
+        
+        return rowId
+
+    def hasExtras(self, filename):
+        # Start by selecting for the filename (if the filename option is being used)
+        conn = sqlite3.connect(self.databasefile)
+        c = conn.cursor()
+
+        c.execute('SELECT count(*) FROM VideoExtrasFileMap where video_source = ?', (filename,))
+        log("Count by file is: " + c.fetchone()[0])
+        
+        # TODO: Need to also check by directory
+        
+        conn.close()
+
+
+
 #########################
 # Main
 #########################
-if len(sys.argv) > 1:
-    videoExtras = VideoExtras(sys.argv[1])
-    videoExtras.run()
+if len(sys.argv) > 2:
+    # Check the first argument to see if it details the type of
+    # command being run
+    
+    # Perform the search command
+    videoExtras = VideoExtras(sys.argv[2])
+    files = videoExtras.findExtras()
+
+    # We are either running the command or just checking for existence
+    # TODO: Can improve performance by not getting all the extras - have an option
+    # for the "findExtras()" method to just search until it finds one
+    if sys.argv[1] == "check":
+        if files:
+            # Set a flag on the window so we know there is data
+            xbmcgui.Window( 12003 ).clearProperty("HideVideoExtrasButton")
+            log("VideoExtras Button Enabled")
+        else:
+            xbmcgui.Window( 12003 ).setProperty( "HideVideoExtrasButton", "true" )
+            log("VideoExtras Button disabled")
+    else:
+        # need to display the extras
+        videoExtras.run(files)
+        extrasDb = ExtrasDB()
+        extrasDb.createDatabase()
+
+
+
+# TODO:
+# Add settings option to clear/remove the database
+# Extract language strings
+
+# Possibly:
+# Option to update advancedsettings.xml
+
+# DialogVideoInfo.xml
+# ===================
+#<onload condition="System.HasAddon(script.videoextras)">XBMC.RunScript(script.videoextras,check,"$INFO[ListItem.FilenameAndPath]")</onload>
+#<control type="button" id="105">
+#    <description>Extras</description>
+#    <include>ButtonInfoDialogsCommonValues</include>
+#    <label>Extras</label>
+#    <onclick>XBMC.RunScript(script.videoextras,display,"$INFO[ListItem.FilenameAndPath]")</onclick>
+#    <visible>System.HasAddon(script.videoextras) + [Container.Content(movies) | Container.Content(episodes) | Container.Content(TVShows)] + IsEmpty(Window(movieinformation).Property("HideVideoExtrasButton"))</visible>
+#</control>
