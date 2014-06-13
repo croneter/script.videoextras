563e25146f33ce27799fb112cff551914a9bb7a4
robwebset@gmail.com <robwebset@users.noreply.github.com>
2014-06-13 14:59:24 +0000

    VideoExtras: Initial versions of Confluence skin files from Gotham 12.1

--- service.py
+++ service.py
@@ -54,11 +54,16 @@ from core import VideoExtrasBase
 # Main class for the Extras Service
 #####################################
 class VideoExtrasService():
+    WIDE_TAG = "_wide"
+    LIST_TAG = "_list"
+    
     def __init__(self):
         # special://skin - This path points to the currently active skin's root directory. 
-        self.skinExtrasOverlay = xbmc.translatePath( "special://skin" ).decode("utf-8")
-        self.skinExtrasOverlay = os_path_join(self.skinExtrasOverlay, "media")
-        self.skinExtrasOverlay = os_path_join(self.skinExtrasOverlay, "videoextras_overlay.png")
+        skinExtrasOverlayBase = xbmc.translatePath( "special://skin" ).decode("utf-8")
+        skinExtrasOverlayBase = os_path_join(skinExtrasOverlayBase, "media")
+        self.skinExtrasOverlay = os_path_join(skinExtrasOverlayBase, "videoextras_overlay.png")
+        self.skinExtrasOverlayWide = os_path_join(skinExtrasOverlayBase, "videoextras_overlay" + VideoExtrasService.WIDE_TAG + ".png")
+        self.skinExtrasOverlayList = os_path_join(skinExtrasOverlayBase, "videoextras_overlay" + VideoExtrasService.WIDE_TAG + ".png")
 
         log("VideoExtrasService: Looking for image overlay file: %s" % self.skinExtrasOverlay)
 
@@ -69,6 +74,26 @@ class VideoExtrasService():
             self.skinExtrasOverlay = os_path_join(self.skinExtrasOverlay, "Default")
             self.skinExtrasOverlay = os_path_join(self.skinExtrasOverlay, "media")
             self.skinExtrasOverlay = os_path_join(self.skinExtrasOverlay, "overlay.png")
+
+        log("VideoExtrasService: Looking for wide image overlay file: %s" % self.skinExtrasOverlayWide)
+
+        if not xbmcvfs.exists(self.skinExtrasOverlayWide):
+            log("VideoExtrasService: No custom wide image, using default")
+            # Add default image setting to skinExtrasOverlay
+            self.skinExtrasOverlayWide = os_path_join(__resource__, "skins")
+            self.skinExtrasOverlayWide = os_path_join(self.skinExtrasOverlayWide, "Default")
+            self.skinExtrasOverlayWide = os_path_join(self.skinExtrasOverlayWide, "media")
+            self.skinExtrasOverlayWide = os_path_join(self.skinExtrasOverlayWide, "overlay" + VideoExtrasService.WIDE_TAG + ".png")
+
+        log("VideoExtrasService: Looking for list image overlay file: %s" % self.skinExtrasOverlayList)
+
+        if not xbmcvfs.exists(self.skinExtrasOverlayList):
+            log("VideoExtrasService: No custom wide image, using default")
+            # Add default image setting to skinExtrasOverlay
+            self.skinExtrasOverlayList = os_path_join(__resource__, "skins")
+            self.skinExtrasOverlayList = os_path_join(self.skinExtrasOverlayList, "Default")
+            self.skinExtrasOverlayList = os_path_join(self.skinExtrasOverlayList, "media")
+            self.skinExtrasOverlayList = os_path_join(self.skinExtrasOverlayList, "overlay" + VideoExtrasService.LIST_TAG + ".png")
 
         self.forceOverlayOverwrite = False
         
@@ -130,14 +155,18 @@ class VideoExtrasService():
                     log("VideoExtrasService: Extras found for (%d) %s" % (item[dbid], item['title']))
                     extrasCacheString = ("%s[%d]%s" % (extrasCacheString, item[dbid], os.linesep))
                     # Add the overlay image for this item
-                    self._createOverlayFile(target, item[dbid])
+                    self._createOverlayFile(target, item[dbid], self.skinExtrasOverlay)
+                    self._createOverlayFile(target, item[dbid], self.skinExtrasOverlayWide, VideoExtrasService.WIDE_TAG)
+                    self._createOverlayFile(target, item[dbid], self.skinExtrasOverlayList, VideoExtrasService.LIST_TAG)
                 else:
                     # No extras so remove the file if it exists
                     self._removeOverlayFile(target, item[dbid])
+                    self._removeOverlayFile(target, item[dbid], VideoExtrasService.WIDE_TAG)
+                    self._removeOverlayFile(target, item[dbid], VideoExtrasService.LIST_TAG)
 
 
     # Calculates where a given overlay file should be
-    def _createTargetPath(self, target, dbid):
+    def _createTargetPath(self, target, dbid, postfix=''):
         # Get the path where the file exists
         rootPath = os_path_join(__profile__, target)
         if not xbmcvfs.exists(rootPath):
@@ -145,13 +174,13 @@ class VideoExtrasService():
             xbmcvfs.mkdirs(rootPath)
         
         # Generate the name of the file that the overlay will be copied to
-        targetFile = os_path_join(rootPath, ("%d.png" % dbid))
+        targetFile = os_path_join(rootPath, ("%d%s.png" % (dbid, postfix)))
         return targetFile
 
     # Creates the overlay file in the expected location
-    def _createOverlayFile(self, target, dbid):
+    def _createOverlayFile(self, target, dbid, srcfile, postfix=''):
         # Generate the name of the file that the overlay will be copied to
-        targetFile = self._createTargetPath(target, dbid)
+        targetFile = self._createTargetPath(target, dbid, postfix)
 
         # Check if the file exists
         if xbmcvfs.exists(targetFile) and not self.forceOverlayOverwrite:
@@ -159,15 +188,15 @@ class VideoExtrasService():
 
         try:
             # Now the path exists, need to copy the file over to it, giving it the name of the DBID
-            xbmcvfs.copy(self.skinExtrasOverlay, targetFile)
+            xbmcvfs.copy(srcfile, targetFile)
         except:
             log("VideoExtrasService: Failed to create file: %s" % targetFile)
             log("VideoExtrasService: %s" % traceback.format_exc())
 
     # Removes an overlay
-    def _removeOverlayFile(self, target, dbid):
+    def _removeOverlayFile(self, target, dbid, postfix=''):
         # Generate the name of the file that the overlay will be removed from
-        targetFile = self._createTargetPath(target, dbid)
+        targetFile = self._createTargetPath(target, dbid, postfix)
 
         if xbmcvfs.exists(targetFile):
             try:
